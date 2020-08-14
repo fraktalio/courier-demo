ce4241d4b309459503c87b1512f962c3036a7d98
Ivan Dugalic <ivan.dugalic@axoniq.io>
2020-08-14 18:06:21 +0200

    first commit

---
+++ src/main/java/com/fraktalio/courier/CourierApplication.java
@@ -0,0 +1,13 @@
+package com.fraktalio.courier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fraktalio/courier/CourierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fraktalio.courier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
