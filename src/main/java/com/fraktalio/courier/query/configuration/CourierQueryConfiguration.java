b9732fb63f4083bb718fa53e0c705ee3d529c055
Ivan Dugalic <ivan.dugalic@axoniq.io>
2020-09-20 01:24:33 +0200

    Configuration enhanced

---
+++ src/main/java/com/fraktalio/courier/query/configuration/CourierQueryConfiguration.java
@@ -0,0 +1,19 @@
+package com.fraktalio.courier.query.configuration;
+
+import org.axonframework.messaging.interceptors.LoggingInterceptor;
+import org.axonframework.queryhandling.QueryBus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CourierQueryConfiguration {
+
+    /************************************************/
+    /* Register interceptors on the bus */
+    /************************************************/
+
+    @Autowired
+    public void registerQueryInterceptorsOnBus(QueryBus queryBus) {
+        queryBus.registerHandlerInterceptor(new LoggingInterceptor<>());
+    }
+}
