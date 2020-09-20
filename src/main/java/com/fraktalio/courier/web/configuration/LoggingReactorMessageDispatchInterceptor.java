d43ca4db43fca1be864dee96a6d33ab3d3da3d8a
Ivan Dugalic <ivan.dugalic@axoniq.io>
2020-09-20 12:20:12 +0200

    Configuration enhanced

---
+++ src/main/java/com/fraktalio/courier/web/configuration/LoggingReactorMessageDispatchInterceptor.java
@@ -0,0 +1,18 @@
+package com.fraktalio.courier.web.configuration;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.extensions.reactor.messaging.ReactorMessageDispatchInterceptor;
+import org.axonframework.messaging.Message;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+public class LoggingReactorMessageDispatchInterceptor<M extends Message<?>> implements ReactorMessageDispatchInterceptor<M> {
+
+    @Override
+    public Mono<M> intercept(Mono<M> message) {
+        return message.doOnNext(m -> {
+            log.info("Dispatched message " + m.getPayload().getClass() + "with metadata " + m.getMetaData());
+        });
+    }
+}
