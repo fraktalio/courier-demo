5f84cde85d43e8525281ae7cd0bafb43568d68f4
Ivan Dugalic <ivan.dugalic@axoniq.io>
2021-01-29 21:49:32 +0100

    configured the retry scheduler on the command gateway
     - it will increase resilience of the system

--- src/main/java/com/fraktalio/courier/web/configuration/CourierWebConfiguration.java
+++ src/main/java/com/fraktalio/courier/web/configuration/CourierWebConfiguration.java
@@ -1,18 +1,22 @@
 package com.fraktalio.courier.web.configuration;
 
+import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.commandhandling.gateway.ExponentialBackOffIntervalRetryScheduler;
+import org.axonframework.extensions.reactor.commandhandling.gateway.DefaultReactorCommandGateway;
 import org.axonframework.extensions.reactor.commandhandling.gateway.ReactorCommandGateway;
 import org.axonframework.extensions.reactor.queryhandling.gateway.ReactorQueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
 
 @Configuration
 class CourierWebConfiguration {
 
     /***************************************************/
     /* Register a dispatch interceptors on the gateway */
-
     /***************************************************/
-
     @Autowired
     void registerCommandInterceptorsOnReactiveGateway(ReactorCommandGateway reactorCommandGateway) {
         reactorCommandGateway.registerDispatchInterceptor(new SpringSecurityReactorMessageDispatchInterceptor());
@@ -24,4 +28,28 @@ class CourierWebConfiguration {
         reactorQueryGateway.registerDispatchInterceptor(new SpringSecurityReactorMessageDispatchInterceptor());
         reactorQueryGateway.registerDispatchInterceptor(new LoggingReactorMessageDispatchInterceptor());
     }
+
+    /***************************************************/
+    /* The RetryScheduler is capable of scheduling retries when command execution has failed.
+    When a command fails due to an exception that is explicitly non-transient, no retries are done at all.
+    Note that the retry scheduler is only invoked when a command fails due to a RuntimeException.
+    Checked exceptions are regarded as a "business exception" and will never trigger a retry. */
+    /***************************************************/
+    @Bean
+    public ReactorCommandGateway reactiveCommandGateway(CommandBus commandBus) {
+        var scheduledExecutorService = Executors.newScheduledThreadPool(5);
+        var retryScheduler = ExponentialBackOffIntervalRetryScheduler
+                .builder()
+                .retryExecutor(scheduledExecutorService)
+                .maxRetryCount(3)
+                .backoffFactor(1000)
+                .build();
+
+        return DefaultReactorCommandGateway.builder()
+                                           .commandBus(commandBus)
+                                           .retryScheduler(retryScheduler)
+                                           .build();
+    }
+
+    //TODO configure retry scheduler for query gateways: https://github.com/AxonFramework/AxonFramework/issues/1692
 }
